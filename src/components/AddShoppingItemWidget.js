221ed78be8665b2800fec9eaf33ac96d9281dac1
leo <lioryarkon9@gmail.com>
2020-10-18 16:24:13 +0300

    AddShoppingItemWidget component; added packages for dropdown and datepicker; connected to 'redux'

--- src/components/AddShoppingItemWidget.js
+++ src/components/AddShoppingItemWidget.js
@@ -1,44 +1,108 @@
 import React, { useState } from "react";
+import Dropdown from "react-dropdown";
+import { connect } from "react-redux";
+import DatePicker from "react-datepicker";
 import styled from "styled-components";
+
+import "react-dropdown/style.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+import { SMALL_SCREEN } from "../theme";
+
+import {
+  onlineStoresSelector,
+  currentCurrencySelector,
+} from "../redux/selectors";
+import { setBoughtItem } from "../redux/actions/boughtItems.actions";
 
 import { ListItem, ListCell } from "./commonStyled";
 
-const AddShoppingItemWidget = () => {
+const AddShoppingItemWidget = ({
+  onlineStores,
+  currentCurrency,
+  setBoughtItem,
+}) => {
   const [name, setName] = useState("");
   const [onlineStoreId, setOnlineStoreId] = useState("");
   const [price, setPrice] = useState("");
   const [deliveryEstimationDate, setDeliveryEstimationDate] = useState("");
 
-  const onTextInputChange = (event, setPropertyFunction) => {
-    setPropertyFunction(event.currentTarget.value);
+  const confirmShoppingItem = () => {
+    setBoughtItem({
+      name,
+      onlineStoreId,
+      price,
+      currencyId: currentCurrency.id,
+      deliveryEstimationDate,
+    });
   };
 
   return (
-    <ListItem>
-      <ListCell>
-        <TextInput
-          value={name}
-          onChange={(event) => onTextInputChange(event, setName)}
-          placeholder="Item Name"
-        />
-      </ListCell>
-      <ListCell>onlineStoreId</ListCell>
-      <ListCell>
-        <TextInput
-          type="number"
-          value={price}
-          onChange={(event) => onTextInputChange(event, setPrice)}
-          placeholder="Price"
-        />
-      </ListCell>
-      <ListCell>deliveryEstimationDate</ListCell>
-    </ListItem>
+    <>
+      <ListItem>
+        <ListCell>
+          <input
+            value={name}
+            onChange={(event) => setName(event.currentTarget.value)}
+            placeholder="New Item Name"
+          />
+        </ListCell>
+        <ListCell>
+          <Dropdown
+            value={onlineStoreId}
+            options={Object.values(onlineStores).map(({ id, name }) => ({
+              value: id,
+              label: name,
+            }))}
+            onChange={({ value }) => setOnlineStoreId(value)}
+            placeholder="Store bought from"
+          />
+        </ListCell>
+        <ListCell>
+          <input
+            type="number"
+            value={price}
+            onChange={(event) => setPrice(event.currentTarget.value)}
+            placeholder="Item's Price"
+          />
+        </ListCell>
+        <ListCell>
+          <DatePicker
+            placeholderText="Due to arrive on"
+            value={deliveryEstimationDate}
+            selected={
+              deliveryEstimationDate
+                ? new Date(deliveryEstimationDate)
+                : new Date()
+            }
+            onChange={(selectedDate) =>
+              setDeliveryEstimationDate(selectedDate.toJSON())
+            }
+            minDate={new Date()}
+          />
+        </ListCell>
+      </ListItem>
+
+      <Confirm>
+        <button onClick={confirmShoppingItem}>Confirm</button>
+      </Confirm>
+    </>
   );
 };
 
-const TextInput = styled.input`
-  width: 100%;
-  box-sizing: border-box;
+const Confirm = styled.div`
+  text-align: center;
+
+  @media ${SMALL_SCREEN} {
+    width: 100%;
+  }
 `;
 
-export default AddShoppingItemWidget;
+const mapStateToProps = (state) => ({
+  onlineStores: onlineStoresSelector(state),
+  currentCurrency: currentCurrencySelector(state),
+});
+
+export default connect(mapStateToProps, { setBoughtItem })(
+  AddShoppingItemWidget
+);
